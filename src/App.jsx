dfd3376879d1a3a364635f2bc7581bb0dff4b420
Retr0 <nanayawbee2014@gmail.com>
2025-09-05 11:45:29 +0000

    done with meals app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // import Counter from "./Components/Counter";
 // import Todo from "./Components/Todo";
 
+import Meals from "./Components/Meals";
+
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <Todo /> */}
+      <Meals />
     </div>
   );
 };
