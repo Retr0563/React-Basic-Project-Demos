a8da7f1d0d8f89f7e86ee4de3c9a5ff9b45696d4
Retr0 <nanayawbee2014@gmail.com>
2025-09-06 04:25:43 +0000

    done with testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 // import Counter from "./Components/Counter";
 // import Todo from "./Components/Todo";
 // import Meals from "./Components/Meals";
-
-import BackgroundToggle from "./Components/BackgroundToggle";
-
+// import BackgroundToggle from "./Components/BackgroundToggle";
 // import Calculator from "./Components/Calculator";
+// import HiddenSearchBar from "./Components/HiddenSearchBar";
+
+import Testimonials from "./Components/Testimonials";
 
 const App = () => {
   return (
@@ -13,7 +14,9 @@ const App = () => {
       {/* <Todo /> */}
       {/* <Meals /> */}
       {/* <Calculator /> */}
-      <BackgroundToggle />
+      {/* <BackgroundToggle /> */}
+      {/* <HiddenSearchBar /> */}
+      <Testimonials />
     </div>
   );
 };
